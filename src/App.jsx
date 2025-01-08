42348001c01f158a1c14a293d080cc8a73f52497
DaikoDev <cdanieleon28@gmail.com>
2025-01-07 19:27:10 -0500

    feat: Quite llamada a svg eliminados

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { HStack } from "@chakra-ui/react";
-import { Button } from "@/components/ui/button";
+import { Button } from "@/core/ui/button";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <HStack>
