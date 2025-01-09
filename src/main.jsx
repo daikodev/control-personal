3fbbbe89e44964d1b51776cc87b37e0401a128d5
DaikoDev <cdanieleon28@gmail.com>
2025-01-09 18:47:07 -0500

    feat: Agregue importacion de bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 import "./index.css";
 import App from "./App.jsx";
-import { ChakraProvider } from "@chakra-ui/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
