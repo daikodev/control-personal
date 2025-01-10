9b138cb57ae36f7202e7111aad077127021e92b7
DaikoDev <cdanieleon28@gmail.com>
2025-01-09 23:27:08 -0500

    feat: Configure las rutas principales de la aplicacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AuthLogin from "./auth/AuthLogin";
+import Dashboard from "./pages/Dashboard";
+import ProtectedRoute from "./routes/ProtectedRoute";
 
 function App() {
   return (
     <>
-      <div>hola</div>
+      <div className="container-fluid p-0 vh-100">
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={<AuthLogin />}></Route>
+            <Route
+              exact
+              path="Dashboard"
+              element={
+                <ProtectedRoute>
+                  <Dashboard />
+                </ProtectedRoute>
+              }
+            ></Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
     </>
   );
 }
