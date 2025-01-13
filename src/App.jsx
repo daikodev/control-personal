e11fbfe72b7838465de4cc3e4832922b34118751
DaikoDev <cdanieleon28@gmail.com>
2025-01-13 10:53:10 -0500

    feat: Se importa el css que modifica los estilos de Chakra UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import "./CustomChakraStyles.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AuthLogin from "./auth/AuthLogin";
 import Dashboard from "./pages/Dashboard";
