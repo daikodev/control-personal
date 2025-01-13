7d8a15dd22ad9b360fb3191a2fdcbf52b8ba243c
DaikoDev <cdanieleon28@gmail.com>
2025-01-13 10:45:53 -0500

    build: Añadi la fuente de geist-sans

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import "./index.css";
 import App from "./App.jsx";
+import "@fontsource/geist-sans";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
