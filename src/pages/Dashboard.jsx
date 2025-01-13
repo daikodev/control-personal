eaa0e4ae467d13dc8282ac3665a2d5eb60a68de5
DaikoDev <cdanieleon28@gmail.com>
2025-01-13 10:51:51 -0500

    feat: Se elimina el codigo de estilos innecesario

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,19 +1,17 @@
 import React from "react";
 import CustomTable from "./components/Table";
-import test from "../assets/test.jpeg";
+import Navbar from "./components/Navbar";
 
 function Dashboard() {
   return (
-    <section className="row g-0 vh-100">
-      <div className="col-xl-3 d-none d-sm-inline d-sm-none d-xl-block">
-        <img src={test} alt="..." className="img-fluid w-100 vh-100" />
-      </div>
-      <div className="col-xl-9 justify-content-center align-items-start mt-5">
+    <>
+      <main>
+        <Navbar></Navbar>
         <CustomTable></CustomTable>
-      </div>
-    </section>
+      </main>
+      ;
+    </>
   );
-
 }
 
 export default Dashboard;
