6a1cf1ef2c280ff7b0c3744502cc0f8f370b9ecc
zareangel <miguelaz1@outlook.com>
2025-01-16 18:58:53 -0500

    Se agrego popup de activar y desactivar y correccion de poner en hidden estado en editar

---
+++ src/pages/components/modals/Activate.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { useDisclosure } from '@chakra-ui/react';
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Button,
+  AlertDialog,
+  AlertDialogOverlay,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogBody,
+  AlertDialogFooter,
+} from '@chakra-ui/react';
+import { CheckIcon } from '@chakra-ui/icons';
+import DotsIcon from "../../../icons/DotsIcon";
+
+function Activate() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const cancelRef = React.useRef();
+
+  return (
+    <div>
+      <Menu>
+        <MenuButton as={Button}  rightIcon={<DotsIcon />}></MenuButton >
+        <MenuList>
+          <MenuItem color="green" onClick={onOpen}>
+            <CheckIcon me="2.5" />
+            Activar
+          </MenuItem>
+        </MenuList>
+      </Menu>
+
+      {/* Dialogo de confirmación */}
+      <AlertDialog
+        isOpen={isOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+        isCentered  // Asegura que la alerta esté centrada
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize="lg" fontWeight="bold">
+              Activar Cliente
+            </AlertDialogHeader>
+            <AlertDialogBody>
+              ¿Está seguro de activar a este empleado?
+            </AlertDialogBody>
+            <AlertDialogFooter>
+              <Button ref={cancelRef} onClick={onClose}>
+                Cancelar
+              </Button>
+              <Button colorScheme="green" onClick={onClose} ml={3}>
+                Activar
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+    </div>
+  );
+}
+
+export default Activate;
