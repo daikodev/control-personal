a2b7004b9d1539dc03be0d1d5112501cf8cc2082
DaikoDev <cdanieleon28@gmail.com>
2025-01-30 20:11:05 -0500

    feat: Cambie el nombre de propiedades de los svg

--- src/icons/DotsIcon.jsx
+++ src/icons/DotsIcon.jsx
@@ -9,10 +9,10 @@ function DotsIcon() {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      class="icon icon-tabler icons-tabler-outline icon-tabler-dots"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="icon icon-tabler icons-tabler-outline icon-tabler-dots"
     >
       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
       <path d="M5 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
--- src/icons/ExcelIcon.jsx
+++ src/icons/ExcelIcon.jsx
@@ -9,10 +9,10 @@ function ExcelIcon() {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      class="icon icon-tabler icons-tabler-outline icon-tabler-file-type-xls"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="icon icon-tabler icons-tabler-outline icon-tabler-file-type-xls"
     >
       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
       <path d="M14 3v4a1 1 0 0 0 1 1h4" />
